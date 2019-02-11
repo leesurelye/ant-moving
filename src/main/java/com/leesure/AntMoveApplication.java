c4e3aefed854bac2a305d44d96c2949f6719da2f
lee <lee@gmail.com>
2019-02-11 22:34:34 +0800

    init the project

---
+++ src/main/java/com/leesure/AntMoveApplication.java
@@ -0,0 +1,14 @@
+package com.leesure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AntMoveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AntMoveApplication.class, args);
+	}
+
+}
+
